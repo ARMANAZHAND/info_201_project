c7c1ca4eb95c7daca6ac5e33dad0da772d7aff9d
NIX <yangdanfeng@Nixs-MacBook-Pro.local>
2018-12-03 19:47:05 -0800

    fix observe, but the legend overlaps with the plot sometimes

--- arman-shinyapp/server.R
+++ arman-shinyapp/server.R
@@ -34,20 +34,20 @@ shinyServer(function(input, output, session) {
   ## calculate the percentage of officer dispatched with
   ## the most 15 initial call types
   readData <- reactive({
-    data <- data.frame(read.csv("data/crisis-data.csv", header = TRUE), 
+    full_data <- data.frame(read.csv("data/crisis-data.csv", header = TRUE), 
                        stringAsFactors = FALSE)
     
     
-    dispatched <- filter(data, str_detect(data$CIT.Officer.Dispatched, "Y"))
+    dispatched <- filter(full_data, str_detect(full_data$CIT.Officer.Dispatched, "Y"))
     
-    group_all <- data %>% 
+    group_all <- full_data %>% 
       group_by(.$Initial.Call.Type) %>%
       count() 
     group_all <- group_all[order(-group_all$n), c(1,2)]
     first_15th <- head(group_all, 15)
     
-    data_first_15th <- data %>% 
-      filter(data$Initial.Call.Type %in% first_15th$`.$Initial.Call.Type`)
+    data_first_15th <- full_data %>% 
+      filter(full_data$Initial.Call.Type %in% first_15th$`.$Initial.Call.Type`)
     group_dispatched <- filter(data_first_15th, str_detect(data_first_15th$CIT.Officer.Dispatched, "Y"))
     group_dispatched <- group_by(group_dispatched, group_dispatched$Initial.Call.Type) %>%
       count()
@@ -65,39 +65,40 @@ shinyServer(function(input, output, session) {
   
   ## allow user to select the initial call types
   output$types <- renderUI({
-      data <- readData()
-      checkboxGroupInput("types", "Choose Types(Max 5; Min 2 types)", choices = data$Initial.Call.Type,
-      selected = data$Initial.Call.Type[13:14])
+      data_15 <- readData()
+      checkboxGroupInput("types", "Choose Types(Max 5; Min 2 types)", choices = data_15$Initial.Call.Type,
+      selected = data_15$Initial.Call.Type[4:5])
   })
   
   ## make a limit of how many initial call types the user
   ## can choose. max 5 and min 2 types
   observe({
+      data_15 <- readData()
       if(length(input$types) > 5)
       {
           updateCheckboxGroupInput(session, "types", selected = tail(input$types,5))
       }
       if(length(input$types) < 2)
       {
-          updateCheckboxGroupInput(session, "types", selected = data$Initial.Call.Type[13:14])
+          updateCheckboxGroupInput(session, "types", selected = data_15$Initial.Call.Type[4:5])
       }
   })
   
   ## make a bar plot of the percentage of officer dispatched
   ## regarding to the initial call type.
   output$dispatched <- renderPlot({
-    data <- readData()
+    data_15 <- readData()
     
-    data <- filter(data, data$Initial.Call.Type %in% input$types)
-    par(mar = c(4,4,4,20))
+    data_15 <- filter(data_15, data_15$Initial.Call.Type %in% input$types)
+    par(mar = c(4,4,4,25))
     
     barplot(
-      data$Percentage,
+      data_15$Percentage,
       main = "Percentage of Officer Dispatched", xlab = "Initial Call Type", 
       ylab = "Percentage", 
-      legend = data$Initial.Call.Type, 
-      args.legend = list(title = "Initial Call Type", cex = 0.75,
-                         x = "right", inset=c(-0.80,0),
+      legend = data_15$Initial.Call.Type, 
+      args.legend = list(title = "Initial Call Type", 
+                         x = "right", inset=c(-1.6,0),
                          bty="n", xpd = TRUE),
       col = c("blue", "red", "pink", "black", "yellow"), font.main = 4, font.lab = 4
     )
